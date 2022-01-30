5f014caf108ec524c6c9c2f559fa5ae4483bf79e
Yoriki Yamaguchi <yorikiyamaguchi@gmail.com>
2022-01-31 03:09:38 +0900

    update

--- src/handlers/main.js
+++ src/handlers/main.js
@@ -9,6 +9,18 @@ exports.handler = async (event) => {
       "content-type": [{
         key: "Content-Type",
         value: "text/plain"
+      }],
+      "access-control-request-headers": [{
+        key: "Access-Control-Request-Headers",
+        value: "*"
+      }],
+      "access-control-request-method": [{
+        key: "Access-Control-Request-Method",
+        value: "*"
+      }],
+      "origin": [{
+        key: "Origin",
+        value: "*"
       }]
     },
     body: event.Records[0].cf.request.clientIp,
